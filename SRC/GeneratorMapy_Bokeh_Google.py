1123e91247e8b0d5896e69287558ce8d6e90bb1e
mknorps <maria.knorps@gmail.com>
2018-04-21 10:11:54 +0200

    slider connected to time

--- SRC/GeneratorMapy_Bokeh_Google.py
+++ SRC/GeneratorMapy_Bokeh_Google.py
@@ -23,27 +23,26 @@ df_views = dl.read_views()
 
 
 #ustalamy fokus mapy (np na pierwszy punkt)
-map_options = GMapOptions(lat=list(df_tx.loc[:100].latitude)[0], lng=list(df_tx.loc[:100].longitude)[0], map_type="roadmap", zoom=11)
+map_options = GMapOptions(lat=list(df_tx.loc[:1].latitude)[0], lng=list(df_tx.loc[:1].longitude)[0], map_type="roadmap", zoom=11)
 
 n_points  = 500
 categories_path = path.join('DATA','categories.csv')
 
-data = dl.read_transactions()
+df_transactions = dl.read_transactions()
 categories = pd.read_table(categories_path, names = ['id_kategorii', 'id_rodzica', 'nazwa'])
 
 #data_with_categories  = pd.merge(data, categories, left_on='category_id', right_on='id_kategorii') 
 
 
 source = ColumnDataSource(data={
-    'lon'       : data['longitude'].loc[:n_points],
-    'lat'       : data['latitude'].loc[:n_points],
-    'category'  : data['category_id'].loc[:n_points],
-    'ref'       : data['offer_id'].loc[:n_points]
+    'lon'       : df_transactions['longitude'].loc[:1],
+    'lat'       : df_transactions['latitude'].loc[:1],
+    'category'  : df_transactions['category_id'].loc[:1],
+    'ref'       : df_transactions['offer_id'].loc[:1]
 })
 
 #dodamy wskazowki przy naajechaniu myszą
 hover = HoverTool(tooltips=[
-    ("desc", "@desc"),
     ("lat", "@lat"),
     ("lon", "@lon"),
     ("category", "@category"),
@@ -67,7 +66,6 @@ def update_plot(attr, old, new):
     
     new_data=  dict(lat=list(df_tx.loc[:new_num].latitude),
                    lon=list(df_tx.loc[:new_num].longitude),
-                   desc=["costam", "jeszcze cos", "co innego", "jakis tekst"],
                    ref=list(df_tx.loc[:new_num].offer_id))
     source.data = new_data
     p.title = str(new_num) + " punktow"
@@ -76,27 +74,32 @@ slider = Slider(start=1, end=100, value=n_points, step=1, title="numer of doots:
 slider.on_change('value', update_plot)
 
 #slider czasu
-def update_plot_2(attr, old, new):
+def update_plot_time(attr, old, new):
   # set the `yr` name to `slider.value` and `source.data = new_data`
-  yr = slider_time.value
-  new_data = {
+    yr = slider_time.value
+    data_slice = df_transactions[(df_transactions['ttimestamp']<(min_timestamp+yr)) & (df_transactions['ttimestamp']>(min_timestamp-yr)) ].loc[:100]
+    print(yr, data_slice)
+    new_data = {
+       'lon'       : data_slice['longitude'],
+       'lat'       : data_slice['latitude'],
+       'category'  : data_slice['category_id']
       #'x'       : data.loc[yr].fertility,
       #'y'       : data.loc[yr].life,
       #'country' : data.loc[yr].Country,
       #'pop'     : (data.loc[yr].population / 20000000) + 2,
       #'region'  : data.loc[yr].region,
-  }
-  #source.data = new_data
+    }
+    source.data = new_data
 
 
 # Make a slider object: slider
 min_timestamp = min(df_tx['ttimestamp'].min(), df_views['ttimestamp'].min())
 max_timestamp = max(df_tx['ttimestamp'].max(), df_views['ttimestamp'].max())
 
-slider_time = Slider(start=0, end=max_timestamp-min_timestamp, step=360, value=min_timestamp, title='Timestamp', format="{://360}")
+slider_time = Slider(start=0, end=max_timestamp-min_timestamp, step=3600, value=0, title='Timestamp', format="{://360}")
 
 # Attach the callback to the 'value' property of slider
-slider_time.on_change('value', update_plot)
+slider_time.on_change('value', update_plot_time)
 
 curdoc().add_root(row(widgetbox(slider, slider_time), p))
 
