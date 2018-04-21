8d64f09e31d47c837870e626aef2e06bda83c4a0
mknorps <maria.knorps@gmail.com>
2018-04-21 12:19:14 +0200

    views added, different hover tools

--- SRC/GeneratorMapy_Bokeh_Google.py
+++ SRC/GeneratorMapy_Bokeh_Google.py
@@ -6,11 +6,11 @@ Created on Thu Apr 19 20:19:37 2018
 """
 import random
 from bokeh.io import output_file, show
-from bokeh.models.widgets import DateSlider
+from bokeh.models.widgets import Panel, Tabs
 from bokeh.models import (Button, Slider,
         ColumnDataSource, GMapOptions, 
         HoverTool, TapTool, OpenURL)
-from bokeh.plotting import gmap, curdoc
+from bokeh.plotting import gmap, curdoc,figure
 from bokeh.layouts import column, row, widgetbox
 import pandas as pd
 import data_load as dl
@@ -28,21 +28,39 @@ map_options = GMapOptions(lat=list(df_tx.loc[:1].latitude)[0], lng=list(df_tx.lo
 n_points  = 500
 categories_path = path.join('DATA','categories.csv')
 
-df_transactions = dl.read_transactions()
 categories = pd.read_table(categories_path, names = ['id_kategorii', 'id_rodzica', 'nazwa'])
 
 #data_with_categories  = pd.merge(data, categories, left_on='category_id', right_on='id_kategorii') 
 
 
-source = ColumnDataSource(data={
-    'lon'       : df_transactions['longitude'].loc[:1],
-    'lat'       : df_transactions['latitude'].loc[:1],
-    'category'  : df_transactions['category_id'].loc[:1],
-    'ref'       : df_transactions['offer_id'].loc[:1]
+source_transactions = ColumnDataSource(data={
+    'lon'       : df_tx['longitude'].loc[:1],
+    'lat'       : df_tx['latitude'].loc[:1],
+    'category'  : df_tx['category_id'].loc[:1],
+    'ref'       : df_tx['offer_id'].loc[:1]
+})
+
+source_views = ColumnDataSource(data={
+    'lon'       : df_views['longitude'].loc[:1],
+    'lat'       : df_views['latitude'].loc[:1],
+    'category'  : df_views['category_id'].loc[:1],
+    'ref'       : df_views['offer_id'].loc[:1]
 })
 
 #dodamy wskazowki przy naajechaniu myszą
-hover = HoverTool(tooltips=[
+hover_tx = HoverTool(names = ["tx"], 
+        tooltips=[
+    ("tx", ""),
+    ("lat", "@lat"),
+    ("lon", "@lon"),
+    ("category", "@category"),
+    ("ref", "@offer_id")
+])
+
+#dodamy wskazowki przy naajechaniu myszą
+hover_views = HoverTool(names = ["views"], 
+        tooltips=[
+    ("views", ""),
     ("lat", "@lat"),
     ("lon", "@lon"),
     ("category", "@category"),
@@ -50,47 +68,43 @@ hover = HoverTool(tooltips=[
 ])
 
 #deklarujemy mapę
-p = gmap("AIzaSyDfyuSoaKSveZClEteSEg8kPinO1fAdOc8", map_options, title="aaa", tools=[hover, 'pan', 'wheel_zoom', 'tap', 'box_zoom'])
+p1 = gmap("AIzaSyDfyuSoaKSveZClEteSEg8kPinO1fAdOc8", map_options, title="Views and transactions", tools=[hover_tx, hover_views, 'pan', 'wheel_zoom', 'tap', 'box_zoom'])
 
 #dodamy url'y dla kliknięć na markery
 url = "http://allegro.pl/i@ref.html"
-taptool = p.select(type=TapTool)
+taptool = p1.select(type=TapTool)
 taptool.callback = OpenURL(url=url)
 
-#dodajemy punkty na mapę
-p.circle(x="lon", y="lat", size=10, fill_color="blue", fill_alpha=0.8, source=source)
+#dodajemy punkty transakcji na mapę
+tx_circles = p1.circle(x="lon", y="lat", name="tx", size=10, fill_color="blue", fill_alpha=0.8, source=source_transactions)
+#tx_circles.select(dict(type=HoverTool)).tooltips = {"lat": "@lat","lon": "@lon"}
 
-#funkcja zmieniajaca ilosc punktow (dla slidera)
-def update_plot(attr, old, new):
-    new_num = slider.value
-    
-    new_data=  dict(lat=list(df_tx.loc[:new_num].latitude),
-                   lon=list(df_tx.loc[:new_num].longitude),
-                   ref=list(df_tx.loc[:new_num].offer_id))
-    source.data = new_data
-    p.title = str(new_num) + " punktow"
-    
-slider = Slider(start=1, end=100, value=n_points, step=1, title="numer of doots:")
-slider.on_change('value', update_plot)
+#dodajemy punkty wyswietlen na mapę
+views_squares = p1.square(x="lon", y="lat", name="views", size=10, fill_color="orange", fill_alpha=0.8, source=source_views)
+#views_squares.select(dict(type=HoverTool)).tooltips = { "category": "@category"}
 
 #slider czasu
 def update_plot_time(attr, old, new):
   # set the `yr` name to `slider.value` and `source.data = new_data`
     yr = slider_time.value
-    data_slice = df_transactions[(df_transactions['ttimestamp']<(min_timestamp+yr)) & (df_transactions['ttimestamp']>(min_timestamp-yr)) ].loc[:100]
-    print(yr, data_slice)
-    new_data = {
+    #x_range
+    #y_range
+
+    data_slice = df_tx[(df_tx['ttimestamp']<(min_timestamp+yr)) & (df_tx['ttimestamp']>(min_timestamp-yr)) ].loc[:100]
+    new_data_transactions = {
        'lon'       : data_slice['longitude'],
        'lat'       : data_slice['latitude'],
        'category'  : data_slice['category_id']
-      #'x'       : data.loc[yr].fertility,
-      #'y'       : data.loc[yr].life,
-      #'country' : data.loc[yr].Country,
-      #'pop'     : (data.loc[yr].population / 20000000) + 2,
-      #'region'  : data.loc[yr].region,
     }
-    source.data = new_data
+    source_transactions.data = new_data_transactions
 
+    data_slice = df_views[(df_views['ttimestamp']<(min_timestamp+yr)) & (df_views['ttimestamp']>(min_timestamp-yr)) ].loc[:100]
+    new_data_views = {
+       'lon'       : data_slice['longitude'],
+       'lat'       : data_slice['latitude'],
+       'category'  : data_slice['category_id']
+    }
+    source_views.data = new_data_views
 
 # Make a slider object: slider
 min_timestamp = min(df_tx['ttimestamp'].min(), df_views['ttimestamp'].min())
@@ -101,12 +115,13 @@ slider_time = Slider(start=0, end=max_timestamp-min_timestamp, step=3600, value=
 # Attach the callback to the 'value' property of slider
 slider_time.on_change('value', update_plot_time)
 
-curdoc().add_root(row(widgetbox(slider, slider_time), p))
+tab1 = Panel(child=p1, title="Map")
 
+p2 = figure(plot_width = 300, plot_height=300)
 
-# Make a row layout of widgetbox(slider) and plot and add it to the current document
-#date_range_slider = DateRangeSlider(title="Date Range: ", start=date(2017, 1, 1), end=date.today(), value=(date(2017, 9, 7), date(2017, 10, 15)), step=1)
-#curdoc().add_root(column(date_range_slider))
+tab2 = Panel(child=p2, title="Events count")
 
+tabs = Tabs(tabs=[tab1,tab2])
 
-#show(p)
+curdoc().add_root(row(widgetbox(slider_time), tabs))
+
